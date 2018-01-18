11c06a634d79477d358f07e32f99ac508258e9b1
abroccoli <acbrocco@syr.edu>
2018-01-18 14:34:33 -0500

    add json to assests and add events components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { EventsComponent } from './events/events.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EventsComponent
   ],
   imports: [
     BrowserModule
